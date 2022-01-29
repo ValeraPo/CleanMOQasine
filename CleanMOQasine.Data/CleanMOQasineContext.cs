374ce89f4821ae173939473c0d4b708c9e731c20
Aufniy Tiger <golenkotoxa@gmail.com>
2022-01-29 20:05:17 +0300

    added context

---
+++ CleanMOQasine.Data/CleanMOQasineContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CleanMOQasine.Data
+{
+    public class CleanMOQasineContext: DbContext
+    {
+        private readonly string _connectionString = @"Server=(localdb);Database=CleanMOQasine;Trusted_Connection=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}

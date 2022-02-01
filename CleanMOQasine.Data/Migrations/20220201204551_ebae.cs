1e7b9b94bb93b54c18444a9281bb3149baf12a00
Dinar <doouw@mail.ru>
2022-02-02 02:22:18 +0500

    FirstRepo

---
+++ CleanMOQasine.Data/Migrations/20220201204551_ebae.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanMOQasine.Data.Migrations
+{
+    public partial class ebae : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ CleanMOQasine.Data/Repositories/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMOQasine.Data.Repositories
+{
+    internal class Class1
+    {
+
+        BaseRepositories zal;
+        public void Aaa()
+        {
+            BaseRepositories qq = BaseRepositories.GetInstance();
+            //qq.c
+        }
+    }
+}

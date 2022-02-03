e8cdc540c52f729b093ffb141c6fc36161ca3f62
Lekksha <bennykorovin@gmail.com>
2022-02-03 22:39:46 +0300

    UserService was created

---
+++ CleanMOQasine.Business/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using CleanMOQasine.Business.Configurations;
+using CleanMOQasine.Business.Models;
+using CleanMOQasine.Data.Repositories;
+
+namespace CleanMOQasine.Business.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _userRepository;
+
+        public UserService()
+        {
+            _userRepository = new UserRepository();
+        }
+
+        public List<UserModel> GetAllAdmins()
+        {
+            var users = _userRepository.GetUsers();
+            return AutoMapperToData.GetInstance().Map<List<UserModel>>(users).Where(u => u.Role == 1).ToList();
+        }
+
+        public List<UserModel> GetAllCleaners()
+        {
+            var users = _userRepository.GetUsers();
+            return AutoMapperToData.GetInstance().Map<List<UserModel>>(users).Where(u => u.Role == 2).ToList();
+        }
+
+        public List<UserModel> GetAllClients()
+        {
+            var users = _userRepository.GetUsers();
+            return AutoMapperToData.GetInstance().Map<List<UserModel>>(users).Where(u => u.Role == 3).ToList();
+        }
+    }
+}
---
+++ CleanMOQasine.Data/Repositories/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using CleanMOQasine.Data.Entities;
+
+namespace CleanMOQasine.Data.Repositories
+{
+    public class UserRepository
+    {
+        private readonly CleanMOQasineContext _dbContext;
+
+        public UserRepository() => _dbContext = CleanMOQasineContext.GetInstance();
+
+        public User? GetUserById(int id) => _dbContext.User.FirstOrDefault(u => u.Id == id);
+
+        public List<User> GetUsers() => _dbContext.User.Where(u => !u.IsDeleted).ToList();
+
+        public User? GetUserByLogin(string login) => _dbContext.User.FirstOrDefault(u => u.Login == login);
+
+        public void AddUser(User user)
+        {
+            _dbContext.User.Add(user);
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateUser(User user)
+        {
+            var entity = GetUserById(user.Id);
+            entity.FirstName = user.FirstName;
+            entity.LastName = user.LastName;
+            entity.Login = user.Login;
+            entity.PhoneNumber = user.PhoneNumber;
+            entity.Rank = user.Rank;
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateUser(int id, bool isDeleted)
+        {
+            var user = GetUserById(id);
+            user.IsDeleted = isDeleted;
+            _dbContext.SaveChanges();
+        }
+    }
+}

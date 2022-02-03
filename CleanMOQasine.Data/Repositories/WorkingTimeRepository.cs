356d5d03847589e2d8ddf8a6643b0a5a6e2df765
Dinar <doouw@mail.ru>
2022-02-03 17:02:00 +0500

    WorkingTime
    Repository, Model and Controller

--- CleanMOQasine.Data/Repositories/WorkingTimeRepository.cs
+++ CleanMOQasine.Data/Repositories/WorkingTimeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanMOQasine.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,11 +15,30 @@ namespace CleanMOQasine.Data.Repositories
         {
             _context = CleanMOQasineContext.GetInstance();
         }
+        public WorkingTime? GetWorkingTimeById(int id)
+           => _context.WorkingTime.FirstOrDefault(g => g.Id == id && !g.IsDeleted);
 
-        //getall
-        //getOne
-        //Update
-        //delete
-        //add
+        public IEnumerable<WorkingTime> GetAllWorkingTimes()
+            => _context.WorkingTime.Where(p => !p.IsDeleted).ToList();
+
+        public void UpdateWorkingTime(WorkingTime workingTime)
+        {
+            var oldWorkingTime = _context.WorkingTime.FirstOrDefault(p => p.Id == workingTime.Id);
+            oldWorkingTime = workingTime;
+            _context.SaveChanges();
+        }
+
+        public void DeleteWorkingTime(int id)
+        {
+            var workingTime = _context.WorkingTime.FirstOrDefault(p => p.Id == id);
+            workingTime.IsDeleted = true;
+            _context.SaveChanges();
+        }
+
+        public void AddWorkingTime(WorkingTime newWorkingTime)
+        {
+            _context.WorkingTime.Add(newWorkingTime);
+            _context.SaveChanges();
+        }
     }
 }

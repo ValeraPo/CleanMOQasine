7da1844db83fb92c9853965df4d834cfe1592f83
Dinar <doouw@mail.ru>
2022-02-03 16:38:13 +0500

    EmptyWorkingTime

---
+++ CleanMOQasine.Data/Repositories/WorkingTimeRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMOQasine.Data.Repositories
+{
+    public class WorkingTimeRepository
+    {
+        private readonly CleanMOQasineContext _context;
+
+        public WorkingTimeRepository()
+        {
+            _context = CleanMOQasineContext.GetInstance();
+        }
+
+        //getall
+        //getOne
+        //Update
+        //delete
+        //add
+    }
+}

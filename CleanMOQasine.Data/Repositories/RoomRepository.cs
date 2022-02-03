39e1d36e9233b1fbf1b6f9dd6f7fafc434a2b436
Lekksha <bennykorovin@gmail.com>
2022-02-03 15:57:22 +0300

    RoomRepository was added

---
+++ CleanMOQasine.Data/Repositories/RoomRepository.cs
@@ -0,0 +1,45 @@
+﻿using CleanMOQasine.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanMOQasine.Data.Repositories
+{
+    public class RoomRepository
+    {
+        private readonly CleanMOQasineContext _dbContext;
+
+        public RoomRepository() => _dbContext = CleanMOQasineContext.GetInstance();
+
+        public Room GetRoomById(int id) => _dbContext.Room.Include(r => r.Orders).FirstOrDefault(r => r.Id == id);
+
+        public List<Room> GetAllRooms() => _dbContext.Room.Include(r => r.Orders).ToList();
+
+        public List<Room> GetRoomsByOrders(Order order) => _dbContext.Room.Where(r => r.Orders.Contains(order)).Include(r => r.Orders).ToList();
+
+        public void AddRoom(Room room)
+        {
+            _dbContext.Room.Add(room);
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateRoom(Room updatedRoom)
+        {
+            var room = GetRoomById(updatedRoom.Id);
+            room = updatedRoom;
+            _dbContext.SaveChanges();
+        }
+
+        public void DeleteRoom(int id)
+        {
+            var room = _dbContext.Room.FirstOrDefault(r => r.Id == id);
+            room.IsDeleted = true;
+            _dbContext.SaveChanges();
+        }
+
+        public void RestoreRoom(int id)
+        {
+            var room = _dbContext.Room.FirstOrDefault(r => r.Id == id);
+            room.IsDeleted = false;
+            _dbContext.SaveChanges();
+        }
+    }
+}

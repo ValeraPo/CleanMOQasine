f1143387f7dffb96cd32e5ee42c0e5e3e408dabb
Dinar <doouw@mail.ru>
2022-02-03 15:41:52 +0500

    Added Payment
    Repository, model and controller

---
+++ CleanMOQasine.Business/Models/PaymentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMOQasine.Business.Models
+{
+    public class PaymentModel
+    {
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+    }
+}
---
+++ CleanMOQasine.Data/Repositories/PaymentRepository.cs
@@ -0,0 +1,49 @@
+﻿using CleanMOQasine.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMOQasine.Data.Repositories
+{
+    public class PaymentRepository
+    {
+        private readonly CleanMOQasineContext _context;
+        public PaymentRepository()
+        {
+            _context = CleanMOQasineContext.GetInstance();
+        }
+
+        //getOne
+        public Payment GetPaymentById (int id) 
+            => _context.Payment.FirstOrDefault(g => g.Id == id && !g.IsDeleted);
+
+        //getall
+        public IEnumerable<Payment> GetAllPayments () 
+            => _context.Payment.Where(p => !p.IsDeleted).ToList();
+
+        //delete
+        public void DeletePayment(int id)
+        {
+            var oldPayment = _context.Payment.FirstOrDefault(p => p.Id == id);
+            oldPayment.IsDeleted = true;
+            _context.SaveChanges();
+        }
+
+        //update
+        public void UpdatePayment(Payment newPayment)
+        {
+            var oldPayment = _context.Payment.FirstOrDefault(p => p.Id == newPayment.Id);
+            oldPayment = newPayment;
+            _context.SaveChanges();
+        }
+
+        //Addone
+        public void AddPayment (Payment newPayment)
+        {
+            _context.Payment.Add(newPayment);
+            _context.SaveChanges();
+        }
+    }
+}

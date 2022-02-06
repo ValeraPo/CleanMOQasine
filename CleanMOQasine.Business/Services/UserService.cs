3ee15cd1d34f1f768ea7328452ae4edc61f55835
Lekksha <bennykorovin@gmail.com>
2022-02-06 21:47:42 +0300

    User services was added

--- CleanMOQasine.Business/Services/UserService.cs
+++ CleanMOQasine.Business/Services/UserService.cs
@@ -24,13 +24,20 @@ namespace CleanMOQasine.Business.Services
             return AutoMapperToData.GetInstance().Map<UserModel>(user);
         }
 
+        public UserModel GetUserByLogin(string login)
+        {
+            var user = _userRepository.GetUserByLogin(login);
+
+            if (user is null)
+                throw new Exception($"Пользователь с логином '{login}' не найден");
+
+            return AutoMapperToData.GetInstance().Map<UserModel>(user);
+        }
+
         public void UpdateUser(int id, UserModel userModel)
         {
             var user = _userRepository.GetUserById(id);
-
-            if (user is null)
-                throw new Exception($"Пользователь с id = {id} не найден");
-
+            CheckUser(user, id);
             var mappedUser = AutoMapperToData.GetInstance().Map<User>(userModel);
             _userRepository.UpdateUser(mappedUser);
         }
@@ -38,19 +45,54 @@ namespace CleanMOQasine.Business.Services
         public List<UserModel> GetAllAdmins()
         {
             var users = _userRepository.GetUsers();
+            CheckListOfUsers(users);
             return AutoMapperToData.GetInstance().Map<List<UserModel>>(users).Where(u => u.Role == Role.Admin).ToList();
         }
 
         public List<UserModel> GetAllCleaners()
         {
             var users = _userRepository.GetUsers();
+            CheckListOfUsers(users);
             return AutoMapperToData.GetInstance().Map<List<UserModel>>(users).Where(u => u.Role == Role.Cleaner).ToList();
         }
 
         public List<UserModel> GetAllClients()
         {
             var users = _userRepository.GetUsers();
+            CheckListOfUsers(users);
             return AutoMapperToData.GetInstance().Map<List<UserModel>>(users).Where(u => u.Role == Role.Client).ToList();
+        }
+
+        public void AddUser(UserModel userModel)
+        {
+            var mappedUser = AutoMapperToData.GetInstance().Map<User>(userModel);
+            _userRepository.AddUser(mappedUser);
+        }
+
+        public void DeleteUserById(int id)
+        {
+            var user = _userRepository.GetUserById(id);
+            CheckUser(user, id);
+            _userRepository.UpdateUser(id, true);
+        }
+
+        public void RestoreUserById(int id)
+        {
+            var user = _userRepository.GetUserById(id);
+            CheckUser(user, id);
+            _userRepository.UpdateUser(id, false);
+        }
+
+        private void CheckUser(User user, int id)
+        {
+            if (user is null)
+                throw new Exception($"Пользователь с id = {id} не найден");
+        }
+
+        private void CheckListOfUsers(List<User> users)
+        {
+            if (users is null) 
+                throw new Exception($"В базе данных нет ни одного пользователя");
         }
     }
 }

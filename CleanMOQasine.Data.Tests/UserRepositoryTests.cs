5bd2c97a3b4cf7138b3ff56e6ac9bf4ed646bd21
Lekksha <bennykorovin@gmail.com>
2022-02-07 20:31:47 +0300

    some tests added

--- CleanMOQasine.Data.Tests/UserRepositoryTests.cs
+++ CleanMOQasine.Data.Tests/UserRepositoryTests.cs
@@ -74,6 +74,43 @@ namespace CleanMOQasine.Data.Tests
             Assert.IsTrue(actual.WorkingHours.Count > 0);
         }
 
+        [Test]
+        public void GetUsers_ShoudReturnUsersWithRelativeEntities()
+        {
+            //given
+            var usersForTest = _userTestData.GetListOfUsersForTests();
 
+            foreach (var user in usersForTest)
+            {
+                _dbContext.Users.Add(user);
+            }
+            _dbContext.SaveChanges();
+
+            //when
+            var actual = _userRepository.GetUsers();
+
+            //then
+            Assert.IsNotNull(actual);
+            Assert.IsTrue(actual.Count > 0);
+            Assert.IsNotNull(actual[0].Orders);
+            Assert.IsTrue(actual[0].Orders.Count > 0);
+            Assert.IsNotNull(actual[0].CleaningAdditions);
+            Assert.IsTrue(actual[0].CleaningAdditions.Count > 0);
+            Assert.IsNotNull(actual[0].WorkingHours);
+            Assert.IsTrue(actual[0].WorkingHours.Count > 0);
+        }
+
+        [Test]
+        public void AddUser_ShoudAddUserToDb()
+        {
+            //given
+            var userForTest = _userTestData.GetUserForTests();
+
+            //when
+            _userRepository.AddUser(userForTest);
+
+            //then
+            //Assert.IsNotNull(actual);
+        }
     }
 }

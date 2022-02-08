ee54ac76d52277b07dd97fd39dec6d2d9a04bdc8
ValeraPo <portnova2412@gmail.com>
2022-02-08 17:15:16 +0300

    Tests for GET in order

---
+++ CleanMOQasine.Data.Tests/OrderRepositoryTests.cs
@@ -0,0 +1,64 @@
+﻿using CleanMOQasine.Data.Repositories;
+using CleanMOQasine.Data.Tests.TestData;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System.Linq;
+
+namespace CleanMOQasine.Data.Tests
+{
+    public class OrderRepositoryTests
+    {
+        private OrderTestData _orderTestData;
+        private CleanMOQasineContext _context;
+        private IOrderRepository _repository;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<CleanMOQasineContext>()
+                          .UseInMemoryDatabase("Test")
+                          .Options;
+
+            _context = new CleanMOQasineContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            _repository = new OrderRepository(_context);
+            _orderTestData = new OrderTestData();
+
+        }
+
+        [Test]
+        public void GetOrderByIdTest()
+        {
+            // given
+            var expected = _orderTestData.GetOrderForTests();
+            _context.Orders.Add(expected);
+            _context.SaveChanges();
+
+            // when
+            var actual = _repository.GetOrderById(expected.Id);
+
+            // then
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetAllOrdersTest()
+        {
+            // given
+            var expected = _orderTestData.GetListOfOrdersForTests();
+            _context.Orders.AddRange(expected);
+            _context.SaveChanges();
+
+            // when
+            var actual = _repository.GetAllOrders();
+
+            // then
+            CollectionAssert.AreEqual(expected.Where(o => !o.IsDeleted), actual);
+        }
+
+        
+
+
+    }
+}
---
+++ CleanMOQasine.Data.Tests/TestData/OrderTestData.cs
@@ -0,0 +1,64 @@
+﻿using CleanMOQasine.Data.Entities;
+using System;
+using System.Collections.Generic;
+
+
+namespace CleanMOQasine.Data.Tests.TestData
+{
+    public class OrderTestData
+    {
+        public Order GetOrderForTests()
+        {
+            var order = new Order
+            {
+                Address = "Ул. Ленина, 1",
+                Date = new DateTime(2008, 5, 1, 8, 30, 52),
+                IsDeleted = false,
+                Rooms = new List<Room>(),
+                CleaningAdditions = new List<CleaningAddition>(),
+                Cleaners = new List<User>(),
+                Payments = new List<Payment>()
+            };
+            return order;
+        }
+
+        public List<Order> GetListOfOrdersForTests()
+        {
+            List<Order> orders = new List<Order>
+            {
+                new Order
+            {
+                Address = "Ул. Ленина, 1",
+                Date = new DateTime(2008, 5, 1, 8, 30, 52),
+                IsDeleted = false,
+                Rooms = new List<Room>(),
+                CleaningAdditions = new List<CleaningAddition>(),
+                Cleaners = new List<User>(),
+                Payments = new List<Payment>()
+            },
+                new Order
+            {
+                Address = "Ул. Матросова, 2",
+                Date = new DateTime(2022, 2, 1, 8, 30, 52),
+                IsDeleted = true,
+                Rooms = new List<Room>(),
+                CleaningAdditions = new List<CleaningAddition>(),
+                Cleaners = new List<User>(),
+                Payments = new List<Payment>()
+            },
+                new Order
+            {
+                Address = "Ул. Ленина, 1",
+                Date = new DateTime(2022, 2, 2, 8, 30, 52),
+                IsDeleted = false,
+                Rooms = new List<Room>(),
+                CleaningAdditions = new List<CleaningAddition>(),
+                Cleaners = new List<User>(),
+                Payments = new List<Payment>()
+            }
+        };
+
+            return orders;
+        }
+    }
+}

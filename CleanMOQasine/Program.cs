3c3e9c0002a868a94935b0692e7dd8a6a0c98a83
Aufniy Tiger <golenkotoxa@gmail.com>
2022-02-15 15:49:30 +0300

    added button in Swagger

--- CleanMOQasine/Program.cs
+++ CleanMOQasine/Program.cs
@@ -9,44 +9,44 @@ string _connectionStringVariableName = "CONNECTION_STRING";
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Test01", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        Description = "JWT Authorization header using the Bearer scheme."
+
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+});
+
 
 builder.Services.AddCustomAuth();
 
 var connectionString = builder.Configuration.GetValue<string>(_connectionStringVariableName);
 builder.Services.AddDbContext<CleanMOQasineContext>(opt
     => opt.UseSqlServer(connectionString));
-
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Test01", Version = "v1" });
-
-//    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-//    {
-//        Name = "Authorization",
-//        Type = SecuritySchemeType.ApiKey,
-//        Scheme = "Bearer",
-//        BearerFormat = "JWT",
-//        In = ParameterLocation.Header,
-//        Description = "JWT Authorization header using the Bearer scheme."
-
-//    });
-//    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-//                {
-//                    {
-//                          new OpenApiSecurityScheme
-//                          {
-//                              Reference = new OpenApiReference
-//                              {
-//                                  Type = ReferenceType.SecurityScheme,
-//                                  Id = "Bearer"
-//                              }
-//                          },
-//                         new string[] {}
-//                    }
-//                });
-//});
 
 builder.Services.RegisterCleanMOQasineServices();
 builder.Services.RegisterCleanMOQasineRepositories();

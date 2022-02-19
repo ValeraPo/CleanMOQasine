71593f33576b9290acd97053b9ae1f06dd90a264
Aufniy Tiger <golenkotoxa@gmail.com>
2022-02-19 18:23:23 +0300

    added validation for CleaningAdditionInputModel

--- CleanMOQasine/Models/Inputs/CleaningAddition/CleaningAdditionInputModel.cs
+++ CleanMOQasine/Models/Inputs/CleaningAddition/CleaningAdditionInputModel.cs
@@ -1,9 +1,18 @@
-﻿namespace CleanMOQasine.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanMOQasine.API.Models
 {
     public class CleaningAdditionInputModel
     {
+        [Required(ErrorMessage = "Не указано название")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Не указана цена")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Не указано время выполнения")]
+        [RegularExpression(@"^(((0{1}|1{1})[0-9]{1})|(2{1}[0-3]{1}))+\:([0-5]{1}[0-9]{1})", 
+            ErrorMessage = "Время выполнения должно быть указано в формате 'чч:мм'. Максимальное значение 23:59.")]
         public string Duration { get; set; }
     }
 }

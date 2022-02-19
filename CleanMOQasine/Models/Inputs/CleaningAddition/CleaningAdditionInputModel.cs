0ac13f8904dc683cfcc151b31cae6d05f2ca38f9
Aufniy Tiger <golenkotoxa@gmail.com>
2022-02-19 18:34:21 +0300

    added custom attribute

--- CleanMOQasine/Models/Inputs/CleaningAddition/CleaningAdditionInputModel.cs
+++ CleanMOQasine/Models/Inputs/CleaningAddition/CleaningAdditionInputModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CleanMOQasine.API.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace CleanMOQasine.API.Models
 {
@@ -11,8 +12,7 @@ namespace CleanMOQasine.API.Models
         public decimal Price { get; set; }
 
         [Required(ErrorMessage = "Не указано время выполнения")]
-        [RegularExpression(@"^(((0{1}|1{1})[0-9]{1})|(2{1}[0-3]{1}))+\:([0-5]{1}[0-9]{1})", 
-            ErrorMessage = "Время выполнения должно быть указано в формате 'чч:мм'. Максимальное значение 23:59.")]
+        [IsTimeSpan(ErrorMessage = "Время выполнения должно быть указано в формате 'чч:мм'. Максимальное значение 23:59.")]
         public string Duration { get; set; }
     }
 }

f1e73a4f2284fafff3974db8cb4252dfd9c1456d
Aufniy Tiger <golenkotoxa@gmail.com>
2022-02-21 14:02:37 +0300

    rating 1-5

--- CleanMOQasine/Models/Inputs/GradeBaseInputModel.cs
+++ CleanMOQasine/Models/Inputs/GradeBaseInputModel.cs
@@ -1,9 +1,13 @@
-﻿namespace CleanMOQasine.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanMOQasine.API.Models
 {
     public class GradeBaseInputModel
     {
         public bool IsAnonymous { get; set; }
         public string Comment { get; set; }
+
+        [RegularExpression(@"[1-5]{1}", ErrorMessage = "Оценка должна быть от 1 до 5")]
         public int Rating { get; set; }
     }
 }

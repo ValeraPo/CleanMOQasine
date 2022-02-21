0ce8945c95b6643f137d4fda4d8d2fce34333ec1
Aufniy Tiger <golenkotoxa@gmail.com>
2022-02-21 11:35:36 +0300

    tests for AuthService

---
+++ CleanMOQasine.Business.Tests/ServiceTests/TestData/AuthServiceTestData.cs
@@ -0,0 +1,29 @@
+﻿using CleanMOQasine.Business.Models;
+using CleanMOQasine.Business.Security;
+using CleanMOQasine.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMOQasine.Business.Tests.ServiceTests
+{
+    public class AuthServiceTestData
+    {
+        public string ValidPassword = "у2меня2сложный8пароль";
+        public User GetUserForTests()
+        {
+            return new User
+            {
+                FirstName = "Андрей",
+                LastName = "Талашев",
+                Email = "andryuXa@mail.ru",
+                Login = "WaspionVoronezh",
+                PhoneNumber = "228",
+                Password = PasswordHash.HashPassword(ValidPassword),
+                Role = Data.Enums.Role.Admin
+            };
+        }
+    }
+}
---
+++ CleanMOQasine.Business/Security/IPasswordHash.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanMOQasine.Business.Security
+{
+    public interface IPasswordHash
+    {
+        string HashPassword(string password);
+        bool ValidatePassword(string password, string correctHash);
+    }
+}
